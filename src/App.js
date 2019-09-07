4e696fbfbdec73e6e823063a221ab035e75179b3
Rahimu Kasibante <rahimuk@stud.ntnu.no>
2019-09-07 23:54:37 +0200

    Issue #13 - Navbar-Layout: Made changes to navbar and added fontawesome.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import Navbar from './components/navbar/Navbar'
-import './stylesheets/navbar.css'             /* Navbar styles */
-
+import './stylesheets/navbar.css'
+import NavbarMedia from "./components/navbar/Navbarmedia";             /* Navbar styles */
+import "@fortawesome/fontawesome-free/css/all.min.css";  /* Font-awesome = must run => npm install --save-dev @fortawesome/fontawesome-free*/
 
 function App() {
     return (
         <div className="App">
 
             <div className="navbar">
-                <Navbar>
-                </Navbar>
+                <NavbarMedia categories={["Color", "Art", "Animation"]}>Picture</NavbarMedia>
+                <NavbarMedia categories={["Example1", "Example2", "Example3"]}>Sounds</NavbarMedia>
+                <NavbarMedia categories={["Type1", "Type2", "Type3"]}>Text</NavbarMedia>
+                <div className="container"><i className="far fa-star"/></div>
             </div>
 
         </div>
--- src/components/navbar/Navbarmedia.js
+++ src/components/navbar/Navbarmedia.js
@@ -2,25 +2,16 @@ import React, { Component } from 'react'
 import Checkbox from "../buttons/Checkbox";
 
 class NavbarMedia extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            Categories: ["Category 1", "Category 2", "Category 3"]
-        }
-
-    }
 
     render() {
 
-        const {title} = this.props;
+        const {categories} = this.props;    /* A list of categories */
 
         return (
             <div className="container">
 
-                {title ? <h3>{title}</h3> : "" }
-
-                {this.state.Categories.map((Item, index) => <Checkbox name={Item} key={index}/> )}
+                <h3>{this.props.children}</h3>
+                {categories.map(( Item, index) => <Checkbox name={Item} key={index}/> )}
 
             </div>
         )
