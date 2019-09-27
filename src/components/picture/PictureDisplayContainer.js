64800b1016f86d62a577d00a2ee95dad0990c385
rubensseva <r.sevaldson@gmail.com>
2019-09-27 14:16:35 +0200

    Issue #31 - Hotfix

--- src/components/picture/PictureDisplayContainer.js
+++ src/components/picture/PictureDisplayContainer.js
@@ -2,20 +2,20 @@ import React, {Component} from 'react';
 
 import '../../stylesheets/Slide.css'
 
-let dec1 = 'it2810-30.idi.ntnu.no/media/img/cubes/cubes_1.svg';
-let dec2 = 'it2810-30.idi.ntnu.no/media/img/cubes/cubes_2.svg';
-let dec3 = 'it2810-30.idi.ntnu.no/media/img/cubes/cubes_3.svg';
-let dec4 = 'it2810-30.idi.ntnu.no/media/img/cubes/cubes_4.svg';
+let dec1 = 'media/img/cubes/cubes_1.svg';
+let dec2 = 'media/img/cubes/cubes_2.svg';
+let dec3 = 'media/img/cubes/cubes_3.svg';
+let dec4 = 'media/img/cubes/cubes_4.svg';
 
-let art1 = 'it2810-30.idi.ntnu.no/media/img/shapes/shapes_1.svg';
-let art2 = 'it2810-30.idi.ntnu.no/media/img/shapes/shapes_2.svg';
-let art3 = 'it2810-30.idi.ntnu.no/media/img/shapes/shapes_3.svg';
-let art4 = 't2810-30.idi.ntnu.no/media/img/shapes/shapes_4.svg';
+let art1 = 'media/img/shapes/shapes_1.svg';
+let art2 = 'media/img/shapes/shapes_2.svg';
+let art3 = 'media/img/shapes/shapes_3.svg';
+let art4 = 'media/img/shapes/shapes_4.svg';
 
-let anim1 = 'it2810-30.idi.ntnu.no/media/img/spirals/spiral_cat_1.svg';
-let anim2 = 'it2810-30.idi.ntnu.no/media/img/spirals/spiral_cat_2.svg';
-let anim3 = 'it2810-30.idi.ntnu.no/media/img/spirals/spiral_cat_3.svg';
-let anim4 = 'it2810-30.idi.ntnu.no/media/img/spirals/spiral_cat_4.svg';
+let anim1 = 'media/img/spirals/spiral_cat_1.svg';
+let anim2 = 'media/img/spirals/spiral_cat_2.svg';
+let anim3 = 'media/img/spirals/spiral_cat_3.svg';
+let anim4 = 'media/img/spirals/spiral_cat_4.svg';
 
 
 class PictureDisplayContainer extends Component {
